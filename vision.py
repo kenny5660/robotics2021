c9512e3a654410ce4e84505009c04cd9b60d8424
KHIPSTERcat <cheshko.ilia@gmail.com>
2021-12-19 04:54:07 +1000

    type fix

--- vision.py
+++ vision.py
@@ -12,7 +12,7 @@ def g_count(img, i):
     l = np.array((84,73,11), np.uint8)
     h = np.array((110,255,255), np.uint8)
     mask = cv2.inRange(img,l,h) 
-    count = cv2.countNonZero(mask) * ((40/37)**i)
+    count = cv2.countNonZero(mask) * (((40/37)**i)**2)
     print(count, ":g ")
 
     return count 
@@ -22,7 +22,7 @@ def b_count(img, i):
     l = np.array((113,1143,31), np.uint8)
     h = np.array((122,255,255), np.uint8)
     mask = cv2.inRange(img,l,h) 
-    count = cv2.countNonZero(mask) * ((40/37)**i)
+    count = cv2.countNonZero(mask) * (((40/37)**i)**2)
     print(count, ":b ")
  
     return count
@@ -31,7 +31,7 @@ def y_count(img, i):
     l = np.array((16,172,13), np.uint8)
     h = np.array((51,255,91), np.uint8)
     mask = cv2.inRange(img,l,h) 
-    count = cv2.countNonZero(mask) * ((40/38)**i)
+    count = cv2.countNonZero(mask) * (((40/37)**i)**2)
     print(count, ":y ")
 
     return count 
@@ -74,7 +74,6 @@ def test(img1 ):
     while True:
         img = rotate_image (img1, -15)
         img = img[630:980]
-        img = rotate_image (img[:,2150:2490], -25)[:,35:320]
         
         img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(img_hsv,low,high) 
@@ -95,6 +94,24 @@ def color(y,b,g):
     if (g > b and g > y):
         return 'g'
     return 'g'
+
+def get_type(y,b,g):
+    col = color(y,b,g)
+    if (col == 'b'):
+        if (b > 30000):
+            return "bb"
+        else:
+            return "mb"
+    elif (col == 'g'):
+        if (g < 30000):
+            return "mg"
+        else:
+            return "bg"
+    else:
+        if (y < 32000):
+            return "by"
+        else:
+            return "by"
 
 def queue_to_st(queue):
     st = {}
@@ -138,8 +155,8 @@ def to_qeue(image):
         y = y_count(a.copy(), i)
         b = b_count(a.copy(), i)
         g = g_count(a.copy(), i)
-        color_b = color(y,b,g)
-        queue.append(color_b)
+        type = get_type(y,b,g)
+        queue.append(type)
         i = i + 1
         #print()
     print(queue)
@@ -152,12 +169,12 @@ def to_qeue(image):
 if __name__ == "__main__":
     import os
     if os.name == 'nt':
-        img1 = cv2.imread ("a00.jpg")
+        img1 = cv2.imread ("t0.jpg")
     else:
         os.system('libcamera-jpeg -o main1080.jpg -t 10 --width 2592  --height 1944')
         img1 = cv2.imread("main1080.jpg")
     
-    to_qeue(img1)
+    print(to_qeue(img1))
     #test(img1)
 
     
